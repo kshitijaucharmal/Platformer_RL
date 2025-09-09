b113410bfe8a80b2e627dd79b88cec27d772f6e7
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2025-09-09 19:11:45 +0530

    Imgui setup, font issue will fix later

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,20 @@
 #include "Camera.hpp"
 #include "Constants.hpp"
 #include "LevelGenerator.hpp"
-#include "Platform.hpp"
 #include "raylib.h"
 #include "Player.hpp"
-#include "World.hpp"
+
+#include "imgui.h"
+#include "rlImGui.h"
 
 // NOTE: Gamepad name ID depends on drivers and OS
 #define XBOX_ALIAS_1 "xbox"
 
 // Constants
 int gamepad = 2; // which gamepad to display
+
+// Networking
+#define PORT 8080
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -22,9 +26,21 @@ int main() {
     // Initialization
     //--------------------------------------------------------------------------------------
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);  // Set MSAA 4X hint before windows creation
     InitWindow(Constants::WIDTH, Constants::HEIGHT, "Platformer");
     SetTargetFPS(75);               // Set our game to run at 60 frames-per-second
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    auto io = ImGui::GetIO();
+
+    // Config Flags
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform
+
+    rlImGuiSetup(false);
     //--------------------------------------------------------------------------------------
 
     // GameObjects init
@@ -36,6 +52,7 @@ int main() {
 
 
     lvlGen.GenerateFromImage(ASSET_DIR "/levels/map1.png", &world, &player);
+    bool open = true;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -68,6 +85,15 @@ int main() {
             // -----------------------------------------------------------------------------
             DrawFPS(10, 10);
             // -----------------------------------------------------------------------------
+
+        // start ImGui Conent
+        rlImGuiBegin();
+
+        // show ImGui Content
+        ImGui::ShowDemoWindow(&open);
+
+        // end ImGui Content
+        rlImGuiEnd();
 
         EndDrawing();
         //----------------------------------------------------------------------------------
